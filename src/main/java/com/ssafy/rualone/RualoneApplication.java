cd36b702b8f887d6a2a974918125b5a59280b75e
marugy <gyulife7301@naver.com>
2023-04-29 18:46:27 +0900

    fix : RualoneApplication location

--- src/main/java/com/ssafy/rualone/RualoneApplication.java
+++ src/main/java/com/ssafy/rualone/RualoneApplication.java
@@ -1,4 +1,4 @@
-package com.ssafy.rualone.domain.member;
+package com.ssafy.rualone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
