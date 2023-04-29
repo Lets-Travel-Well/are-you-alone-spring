eaaf800dad8d4f750575b4a64a794ee5bce85799
marugy <gyulife7301@naver.com>
2023-04-29 15:15:05 +0900

    start project

---
+++ src/main/java/com/ssafy/rualone/rualone/RualoneApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.rualone.rualone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RualoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RualoneApplication.class, args);
+	}
+
+}
