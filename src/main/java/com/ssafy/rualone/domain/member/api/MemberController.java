7c412f9630dc3c06114bd817c7d4cca556631026
marugy <gyulife7301@naver.com>
2023-05-01 16:01:25 +0900

    feat : member - logout

--- src/main/java/com/ssafy/rualone/domain/member/api/MemberController.java
+++ src/main/java/com/ssafy/rualone/domain/member/api/MemberController.java
@@ -31,4 +31,9 @@ public class MemberController {
         return "/";
     }
 
+    @GetMapping("logout")
+    public String logout(HttpSession session){
+        session.invalidate();
+        return "redirect:/";
+    }
 }
